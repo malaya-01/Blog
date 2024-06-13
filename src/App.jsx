1f3a1b05fd139a80cdcc58418d40563863655613
malaya-01 <practice.git.963@gmail.com>
2024-06-13 15:07:13 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
 import { Footer, Header } from './components'
-import { Outlet } from 'react-router-dom'
+// import { Outlet } from 'react-router-dom'
 
 function App() {
 
@@ -29,7 +29,7 @@ function App() {
       <div className="w-full block">
         <Header />
         <main>
-          <Outlet />
+          Todo:{/* <Outlet /> */}
         </main>
         <Footer />
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,22 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Home from './components/Home/Home.jsx'
+// import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+// import Home from './components/Home/Home.jsx'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App/>} >
-      <Route path='' element={<Home />} />
-    </Route>
-  )
-)
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<App />} >
+//       <Route path='' element={<Home />} />
+//     </Route>
+//   )
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <RouterProvider router={router}/>
+      {/* <RouterProvider router={router}/> */}
+      <App />
     </React.StrictMode>
   </Provider>
   ,
